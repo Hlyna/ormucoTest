8b5a0f31bc92bc137041908034350ee886cd89f7
Hlyna <lyna@lyna-UX310uqk>
2021-04-12 09:48:13 -0400

    add

--- questionB/librarytest.c
+++ questionB/librarytest.c
@@ -14,9 +14,7 @@ int main(int argc, char **argv){
   		assert(-1 == compare ("bcd","abc"));
   		assert(0  == compare ("7ok","9.0"));
   		assert(2  == compare ("10.87ok","8.2alright"));
-  		
-
-
+ 
 		if(compare(argv[1],argv[2]) == 2) printf("%s is greater than %s\n",argv[1],argv[2]);
 		if(compare(argv[1],argv[2]) == 0) printf("%s is less than %s\n",argv[1],argv[2]);
 		if(compare(argv[1],argv[2]) == 1) printf("%s is equal to %s\n",argv[1],argv[2]);
